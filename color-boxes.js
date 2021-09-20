41b1474269c26aeba9dede74ddcb60747df8486b
evebigaj <eve.bigaj@gmail.com>
2021-09-20 11:56:15 -0400

    hide text

--- color-boxes.js
+++ color-boxes.js
@@ -5,15 +5,19 @@ for (var i=0, max=all.length; i < max; i++) {
      switch (rand){
      case rand = 0: 
      all[i].style.backgroundColor = "red"
+     all[i].style.color = "red"
      break;
      case rand = 1: 
      all[i].style.backgroundColor = "yellow"
+     all[i].style.color = "yellow"
      break;
      case rand = 2: 
      all[i].style.backgroundColor = "blue"
+     all[i].style.color = "blue"
      break;
      case rand = 3: 
      all[i].style.backgroundColor = "white"
+     all[i].style.color = "white"
      break;
      default: break;
     }
