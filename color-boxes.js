4abf6fa1654e8310a69e2ea5f0172a4ad990c985
evebigaj <eve.bigaj@gmail.com>
2021-09-20 12:11:40 -0400

    hide videos

--- color-boxes.js
+++ color-boxes.js
@@ -24,10 +24,16 @@ for (var i=0, max=all.length; i < max; i++) {
 }
 
 for (var i=0, max=all.length; i < max; i++){
-    all[i].style.border = '0.1em solid'}
+    all[i].style.border = '0.1em solid'
+all[i].style.borderColor = "black"}
 
 
 var img = document.getElementsByTagName('img')
 
 for(let i =0, max=img.length; i < max; i++){
 img[i].style.visibility = "hidden"}
+
+var vid = document.getElementsByTagName('video')
+
+for(let i =0, max=vid.length; i < max; i++){
+vid[i].style.visibility = "hidden"}
